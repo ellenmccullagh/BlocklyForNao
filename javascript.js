871fd9f7c0d66fb3c6b1dd5eeb5ce1e848ea7e85
Ellen McCullagh <ellenmccullagh@gmail.com>
2015-12-18 16:45:01 -0800

    Trying to add blockly

---
+++ javascript.js
@@ -0,0 +1,79 @@
+Blockly.JavaScript['page_settings'] = function(block) {
+  var statements_page_header = Blockly.JavaScript.statementToCode(block, 'page_header');
+  var value_color = Blockly.JavaScript.valueToCode(block, 'page_color', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_background = Blockly.JavaScript.valueToCode(block, 'page_background', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_paragraph = Blockly.JavaScript.valueToCode(block, 'page_paragraph', Blockly.JavaScript.ORDER_ATOMIC);
+
+  var code = 'document.body.style.color = "' + value_color + '";\n';
+  code +=    'document.body.style.backgroundColor = "' + value_background + '";\n';
+  code +=    'document.getElementById("description").innerHTML = "' + value_paragraph + '";\n';
+  code +=    statements_page_header;
+  return code;
+};
+
+Blockly.JavaScript['page_header'] = function(block) {
+  var value_header_text = Blockly.JavaScript.valueToCode(block, 'header_text', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_color = Blockly.JavaScript.valueToCode(block, 'page_color', Blockly.JavaScript.ORDER_ATOMIC);
+
+  var code = 'document.getElementById("header").innerHTML = "' + value_header_text + '";\n';
+  code +=    'document.getElementById("header").style.color = "' + value_color + '";\n';
+  return code;
+};
+
+Blockly.JavaScript['color_blue'] = function(block) {
+  var colour_blue = block.getFieldValue('blue');
+
+  var code = '#3333FF';
+
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['color_red'] = function(block) {
+  var colour_red = block.getFieldValue('red');
+
+  var code = '#FF3333';
+
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['color_green'] = function(block) {
+  var colour_green = block.getFieldValue('green');
+
+  var code = '#33FF33';
+
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['color_black'] = function(block) {
+  var colour_black = block.getFieldValue('black');
+
+  var code = '#000000';
+
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['color_white'] = function(block) {
+  var colour_white = block.getFieldValue('white');
+
+  var code = '#FFFFFF';
+
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['page_text'] = function(block) {
+  var text_text = block.getFieldValue('page_text');
+
+  var code = text_text;
+
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['nao_says'] = function(block) {
+    var text_name = block.getFieldValue('NAME');
+    var value_nao_says = Blockly.JavaScript.valueToCode(block, 'Nao Says', Blockly.JavaScript.ORDER_ATOMIC);
+    // TODO: Assemble JavaScript into code variable.
+    var code = 'from naoqi import ALProxy' + '\n'
+    code += 'tts = ALProxy("ALTextToSpeech", "<IP of your robot>", 9559)' +'\n'
+    code += 'tts.say("Hello, world!")';
+    return code;
+};
